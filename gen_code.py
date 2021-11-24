1ea9209afe2da2b1c98006b6499cc14d8ca9cac6
pmcafo <pmcafo@proton.me>
2021-11-24 13:34:21 +0000

    Fixes #35

--- gen_code.py
+++ gen_code.py
@@ -38,4 +38,41 @@ def gen_transpose_common():
                    '}\n'
         content_cpp += template + '\n'
         templat2 = '        else if (transpose_type == TRANS2D_%d%d) {\n' % (t[0], t[1]) + \
-                   '            if (input->dims != %d) {\n' % (2, ) 
+                   '            if (input->dims != %d) {\n' % (2, ) + \
+                   '                printf("Error from transpose op, transpose_type == TRANS2D_%d%d, input->dims != %d\\n");\n' % (t[0], t[1], 2) + \
+                   '                exit(1);\n' + \
+                   '            }\n' + \
+                   '            const int %s = input->shape->at(%d);\n' % (D[0], 0) + \
+                   '            const int %s = input->shape->at(%d);\n' % (D[1], 1) + \
+                   '            transpose2d_%d%d_cpp_kernel<float>(num_threads_, input->data_fp32, output->data_fp32, input->numel, %s, %s);\n' % (t[0], t[1], D[0], D[1]) + \
+                   '        }\n'
+        templat3 = '    else if (transpose_type == TRANS2D_%d%d) {\n' % (t[0], t[1]) + \
+                   '        if (input->dims != %d) {\n' % (2, ) + \
+                   '            printf("Error from transpose op, transpose_type == TRANS2D_%d%d, input->dims != %d\\n");\n' % (t[0], t[1], 2) + \
+                   '            exit(1);\n' + \
+                   '        }\n' + \
+                   '        const int %s = input->shape->at(%d);\n' % (D[0], 0) + \
+                   '        const int %s = input->shape->at(%d);\n' % (D[1], 1) + \
+                   '        output = new SNT Tensor(MMSHAPE2D(%s, %s), FP32, false, false);\n' % (D[t[0]], D[t[1]]) + \
+                   '    }\n'
+        if kkk == 0:
+            templat2 = templat2.replace("else if (", "if (")
+            templat3 = templat3.replace("else if (", "if (")
+        kkk += 1
+        content_cpp_invoke += templat2
+        content_forward += templat3
+
+    # 3d
+    D = ['N', 'H', 'W']
+    d = ['n', 'h', 'w']
+    for t in trans3d:
+        template = 'template<typename data_t>\n' + \
+                   'void transpose3d_%d%d%d_cpp_kernel(const int num_threads_, const data_t* x, data_t* y, int num, int %s, int %s, int %s) {\n' % (t[0], t[1], t[2], D[0], D[1], D[2]) + \
+                   '    // y[%s][%s][%s] = x[%s][%s][%s]\n' % (d[t[0]], d[t[1]], d[t[2]], d[0], d[1], d[2]) + \
+                   '    #pragma omp parallel for num_threads(num_threads_)\n' + \
+                   '    for (int %s = 0; %s < %s; %s++) {\n' % (d[0], d[0], D[0], d[0]) + \
+                   '        for (int %s = 0; %s < %s; %s++) {\n' % (d[1], d[1], D[1], d[1]) + \
+                   '            for (int %s = 0; %s < %s; %s++) {\n' % (d[2], d[2], D[2], d[2]) + \
+                   '                y[((%s * %s) + %s) * %s + %s] = x[((%s * %s) + %s) * %s + %s];\n' % (d[t[0]], D[t[1]], d[t[1]], D[t[2]], d[t[2]],      d[0], D[1], d[1], D[2], d[2]) + \
+                   '            }\n' + \
+             
