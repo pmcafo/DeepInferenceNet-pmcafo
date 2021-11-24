600109a56cd2804581ffea1e0ebcb674e881f6dc
pmcafo <pmcafo@proton.me>
2021-11-24 21:03:57 +0000

    Respond to review comments

--- gen_code.py
+++ gen_code.py
@@ -75,4 +75,39 @@ def gen_transpose_common():
                    '            for (int %s = 0; %s < %s; %s++) {\n' % (d[2], d[2], D[2], d[2]) + \
                    '                y[((%s * %s) + %s) * %s + %s] = x[((%s * %s) + %s) * %s + %s];\n' % (d[t[0]], D[t[1]], d[t[1]], D[t[2]], d[t[2]],      d[0], D[1], d[1], D[2], d[2]) + \
                    '            }\n' + \
-             
+                   '        }\n' + \
+                   '    }\n' + \
+                   '}\n'
+        content_cpp += template + '\n'
+        templat2 = '        else if (transpose_type == TRANS3D_%d%d%d) {\n' % (t[0], t[1], t[2]) + \
+                   '            if (input->dims != %d) {\n' % (3, ) + \
+                   '                printf("Error from transpose op, transpose_type == TRANS3D_%d%d%d, input->dims != %d\\n");\n' % (t[0], t[1], t[2], 3) + \
+                   '                exit(1);\n' + \
+                   '            }\n' + \
+                   '            const int %s = input->shape->at(%d);\n' % (D[0], 0) + \
+                   '            const int %s = input->shape->at(%d);\n' % (D[1], 1) + \
+                   '            const int %s = input->shape->at(%d);\n' % (D[2], 2) + \
+                   '            transpose3d_%d%d%d_cpp_kernel<float>(num_threads_, input->data_fp32, output->data_fp32, input->numel, %s, %s, %s);\n' % (t[0], t[1], t[2], D[0], D[1], D[2]) + \
+                   '        }\n'
+        templat3 = '    else if (transpose_type == TRANS3D_%d%d%d) {\n' % (t[0], t[1], t[2]) + \
+                   '        if (input->dims != %d) {\n' % (3, ) + \
+                   '            printf("Error from transpose op, transpose_type == TRANS3D_%d%d%d, input->dims != %d\\n");\n' % (t[0], t[1], t[2], 3) + \
+                   '            exit(1);\n' + \
+                   '        }\n' + \
+                   '        const int %s = input->shape->at(%d);\n' % (D[0], 0) + \
+                   '        const int %s = input->shape->at(%d);\n' % (D[1], 1) + \
+                   '        const int %s = input->shape->at(%d);\n' % (D[2], 2) + \
+                   '        output = new SNT Tensor(MMSHAPE3D(%s, %s, %s), FP32, false, false);\n' % (D[t[0]], D[t[1]], D[t[2]]) + \
+                   '    }\n'
+        content_cpp_invoke += templat2
+        content_forward += templat3
+
+    # 4d
+    D = ['N', 'C', 'H', 'W']
+    d = ['n', 'c', 'h', 'w']
+    for t in trans4d:
+        template = 'template<typename data_t>\n' + \
+                   'void transpose4d_%d%d%d%d_cpp_kernel(const int num_threads_, const data_t* x, data_t* y, int num, int %s, int %s, int %s, int %s) {\n' % (t[0], t[1], t[2], t[3], D[0], D[1], D[2], D[3]) + \
+                   '    // y[%s][%s][%s][%s] = x[%s][%s][%s][%s]\n' % (d[t[0]], d[t[1]], d[t[2]], d[t[3]], d[0], d[1], d[2], d[3]) + \
+                   '    #pragma omp parallel for num_threads(num_threads_)\n' + \
+                   '    for (int %s = 0; %s < %s; %s++) {\n' 
