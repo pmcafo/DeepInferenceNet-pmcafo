1f2035eff5b09ba77d36eff2cc9de5bc80016e4a
pmcafo <pmcafo@proton.me>
2022-06-09 07:19:04 +0000

    fixed contact form

---
+++ miemiedet/models/necks/custom_pan.h
@@ -0,0 +1,17 @@
+#ifndef __MIEMIEDET_CUSTOM_PAN_H__
+#define __MIEMIEDET_CUSTOM_PAN_H__
+
+#include "../../miemiedet.h"
+#include "../../../miemienet/macros.h"
+#include "../../../miemienet/miemienet.h"
+
+using namespace miemienet;
+
+namespace miemiedet {
+
+class ConvBNLayer;
+
+class SPP : public Layer
+{
+public:
+ 
