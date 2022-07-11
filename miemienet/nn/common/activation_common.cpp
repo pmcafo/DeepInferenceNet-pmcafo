4a66b0a324be96b736d9930b4ba3851762cf3c94
pmcafo <pmcafo@proton.me>
2022-07-11 17:10:09 +0000

    Added Travis-CI

--- miemienet/nn/common/activation_common.cpp
+++ miemienet/nn/common/activation_common.cpp
@@ -74,4 +74,91 @@ void leakyrelu_x86_kernel(const int num_threads_, const data_t* x, data_t* y, fl
 //            for (int i = 0; i < BLOCK; i++) {
 //                for (int j = 0; j < BLOCK; j++) {
 //                    float val = x[((h + j) * W) + (w + i)];
-//                    if (val > sta
+//                    if (val > static_cast<data_t>(0.f))
+//                    {
+//                        y[((h + j) * W) + (w + i)] = val;
+//                    }
+//                    else
+//                    {
+//                        y[((h + j) * W) + (w + i)] = val * alpha;
+//                    }
+//                }
+//            }
+//        }
+//    }
+}
+
+template<typename data_t>
+void leakyrelu_grad_cpp_kernel(const int num_threads_, const data_t* dy, const data_t* x, data_t* dx, float alpha, int num){
+    #pragma omp parallel for num_threads(num_threads_)
+    for (int i = 0; i < num; i++) {
+        if (x[i] > static_cast<data_t>(0.f))
+        {
+            dx[i] = dy[i];
+        }
+        else
+        {
+            dx[i] = dy[i] * alpha;
+        }
+    }
+}
+
+template<typename data_t>
+void relu_cpp_kernel(const int num_threads_, const data_t* x, data_t* y, int num){
+    #pragma omp parallel for num_threads(num_threads_)
+    for (int i = 0; i < num; i++) {
+        if (x[i] > static_cast<data_t>(0.f))
+        {
+            y[i] = x[i];
+        }
+        else
+        {
+            y[i] = static_cast<data_t>(0.f);
+        }
+    }
+}
+
+template<typename data_t>
+void relu_grad_cpp_kernel(const int num_threads_, const data_t* dy, const data_t* x, data_t* dx, int num){
+    #pragma omp parallel for num_threads(num_threads_)
+    for (int i = 0; i < num; i++) {
+        if (x[i] > static_cast<data_t>(0.f))
+        {
+            dx[i] = dy[i];
+        }
+        else
+        {
+            dx[i] = static_cast<data_t>(0.f);
+        }
+    }
+}
+
+// expf instead of exp should be used for float type, complement
+// and register float kernel separatelly
+template<typename data_t>
+void sigmoid_cpp_kernel(const int num_threads_, const data_t* x, data_t* y, int num){
+    #pragma omp parallel for num_threads(num_threads_)
+    for (int i = 0; i < num; i++) {
+        y[i] = static_cast<float>(1.f / (1.f + expf(-x[i])));
+    }
+}
+
+template<typename data_t>
+void sigmoid_grad_cpp_kernel(const int num_threads_, const data_t* dy, const data_t* y, data_t* dx, int num){
+    #pragma omp parallel for num_threads(num_threads_)
+    for (int i = 0; i < num; i++) {
+        dx[i] = dy[i] * y[i] * (1.f - y[i]);
+    }
+}
+
+template<typename data_t>
+void tanh_cpp_kernel(const int num_threads_, const data_t* x, data_t* y, int num){
+    #pragma omp parallel for num_threads(num_threads_)
+    for (int i = 0; i < num; i++) {
+        y[i] = tanhf(x[i]);
+    }
+}
+
+template<typename data_t>
+void tanh_grad_cpp_kernel(const int num_threads_, const data_t* dy, const data_t* y, data_t* dx, int num){
+    #pragma omp parallel for num_threa
