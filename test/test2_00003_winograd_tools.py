e60d8ef49ad4869747b52c62d0592073f7a8e3a2
pmcafo <pmcafo@proton.me>
2024-03-05 07:24:04 +0000

    Submodule Update

---
+++ test/test2_00003_winograd_tools.py
@@ -0,0 +1,24 @@
+import copy
+
+import numpy as np
+
+
+
+
+
+
+# input_name = 'x'
+# output_name = 'xaa'
+
+
+# input_name = 'input'
+# output_name = '            V'
+# B = np.array([[1, 0, 0, 0],
+#               [0, 1, -1, 1],
+#               [-1, 1, 1, 0],
+#               [0, 0, 0, -1]])
+
+
+
+input_name = 'Q'
+output_name = '   
