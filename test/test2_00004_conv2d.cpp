34a11c381d10d11421ba97bcd2b3ff5865da53f3
pmcafo <pmcafo@proton.me>
2024-03-11 15:59:29 +0000

    Added LICENSE.txt

--- test/test2_00004_conv2d.cpp
+++ test/test2_00004_conv2d.cpp
@@ -66,4 +66,91 @@ private:
 int main(int argc, char** argv)
 {
 /*
-python build.py --platform LINUX
+python build.py --platform LINUX --cxx g++ --backend BACKEND_X86 --exec_file test2_00004_conv2d
+
+./test2_00004_conv2d.out
+
+
+*/
+#if defined(WINDOWS)
+    printf("%s\n", "WINDOWS");
+#endif
+#if defined(LINUX)
+    printf("%s\n", "LINUX");
+#endif
+    printf("%s\n", miemienet::miemienetVersion());
+
+    // 修改Config全局配置时，要在网络创建之前修改。
+    Config* cfg = Config::getInstance();
+    printf("num_threads=%d\n", cfg->num_threads);
+    // 图片张量设置为以NHWC顺序排列。这样对cpu计算更友好。
+    // image_data_format会影响卷积层，全连接层的权重的排列方式。
+//    cfg->image_data_format = NCHW;
+    cfg->image_data_format = NHWC;
+    cfg->use_cpp_compute = true;
+    cfg->fuse_conv_bn = true;
+
+    const int num_threads_ = cfg->num_threads;
+
+    char* test_name = "00004";
+
+//    int batch_size = 8;
+//    int input_size = 256;
+//    int in_features = 32;
+//    int out_features = 32;
+//    int kernel_size = 3;
+//    int groups = 1;
+
+    int batch_size = 8;
+    int input_size = 256;
+    int in_features = 32;
+    int out_features = 32;
+    int kernel_size = 3;
+    int groups = 32;
+
+    int stride = 1;
+    int padding = (kernel_size - 1) / 2;
+//    bool bias = false;
+    bool bias = true;
+
+    char file_name[256];
+
+    Model* model = new SNT Model(in_features, out_features, kernel_size, stride, padding, bias, groups);
+
+    std::vector<char*>* param_names = new std::vector<char*>;
+    std::vector<Tensor*>* params = new std::vector<Tensor*>;
+    model->named_parameters(param_names, params);
+    for (int i = 0; i < param_names->size(); i++)
+    {
+        printf("param_names[%d]=%s\n", i, param_names->at(i));
+    }
+
+    model->eval();
+
+    Tensor* x;
+    Tensor* y;
+    if (Config::getInstance()->image_data_format == NCHW)
+    {
+        x = new SNT Tensor(MMSHAPE4D(batch_size, in_features, input_size, input_size), FP32, false, false);
+    }
+    else if (Config::getInstance()->image_data_format == NHWC)
+    {
+        x = new SNT Tensor(MMSHAPE4D(batch_size, input_size, input_size, in_features), FP32, false, false);
+    }
+
+    printf("======================== init ========================\n");
+    x->normal_init(0.f, 1.f, 0);
+    model->conv->weight->normal_init(0.f, 1.f, 1);
+    model->conv->bias->normal_init(0.f, 1.f, 2);
+
+    // 建立计算图，初始化所有中间张量，给所有中间张量分配内存。
+    y = model->create_tensors(x);
+
+    model->conv->weight->print_msg("model->conv->weight");
+    model->conv->weight->print_data(30);
+    model->conv->bias->print_msg("model->conv->bias");
+    model->conv->bias->print_data(30);
+
+    sprintf(file_name, "test/save_data/%s-x.bin", test_name);
+    x->save_as_bin(file_name);
+    sprintf(file_name, "test/save_data/%s-w.bin", test_na
